24e3e6d721df63d373c9502990e943900b4ea24a
Takahiro Itazuri <zulinx86@gmail.com>
2026-03-22 19:48:23 +0000

    feat: activate extension on markdown file open
    - Add onLanguage:markdown activation event
    - Remove manual extension activation from openLink test
    - Open markdown file in test to trigger activation instead
    
    Signed-off-by: Takahiro Itazuri <zulinx86@gmail.com>

--- src/test/openLink.test.ts
+++ src/test/openLink.test.ts
@@ -4,17 +4,16 @@ import * as vscode from 'vscode';
 suite('openLink command', () => {
 	const workspaceUri = vscode.workspace.workspaceFolders![0].uri;
 
-	suiteSetup(async () => {
-		// ensure the extension is activated
-		await vscode.commands.executeCommand('codeAtlas.bookmarkSelection');
-	});
-
 	test('should open file and move cursor to startLine', async () => {
 		const tmpFileUri = vscode.Uri.joinPath(workspaceUri, 'tmp-test-openlink.ts');
 		const content = 'line1\nline2\nline3\nline4\nline5\n';
 		await vscode.workspace.fs.writeFile(tmpFileUri, Buffer.from(content, 'utf-8'));
 
 		try {
+			// open a markdown file to trigger extension activation
+			const mdDoc = await vscode.workspace.openTextDocument({ content: '', language: 'markdown' });
+			await vscode.window.showTextDocument(mdDoc);
+
 			await vscode.commands.executeCommand('codeAtlas.openLink', {
 				filePath: 'tmp-test-openlink.ts',
 				startLine: 3,
