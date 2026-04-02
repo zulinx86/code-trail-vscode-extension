d818fab4d46194e865a5d78090aaf336b79d0b11
Takahiro Itazuri <zulinx86@gmail.com>
2026-04-02 09:16:21 +0100

    chore(logger): Tiny refactor
    Signed-off-by: Takahiro Itazuri <zulinx86@gmail.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 
+// Use "let" to prevent re-definition in the same scope.
 let channel: vscode.OutputChannel;
 
 export function initLogger(): vscode.OutputChannel {
