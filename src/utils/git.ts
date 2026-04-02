89b4138c4aaf4cd77fab1e4c6c0da27d84e65091
Takahiro Itazuri <zulinx86@gmail.com>
2026-04-02 09:11:26 +0100

    chore(git): Tiny refactor
    Signed-off-by: Takahiro Itazuri <zulinx86@gmail.com>

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,14 +1,6 @@
 import * as vscode from 'vscode';
 import { log } from './logger';
 
-function getGitApi(): any | undefined {
-	const gitExtension = vscode.extensions.getExtension('vscode.git');
-	if (!gitExtension?.isActive) {
-		return undefined;
-	}
-	return gitExtension.exports.getAPI(1);
-}
-
 export function remoteUrlToHttps(url: string): string {
 	// git@github.com:user/repo.git -> https://github.com/user/repo
 	const sshMatch = url.match(/^git@([^:]+):(.+?)(?:\.git)?$/);
@@ -17,6 +9,14 @@ export function remoteUrlToHttps(url: string): string {
 	}
 	// https://github.com/user/repo.git -> https://github.com/user/repo
 	return url.replace(/\.git$/, '');
+}
+
+function getGitApi(): any | undefined {
+	const gitExtension = vscode.extensions.getExtension('vscode.git');
+	if (!gitExtension?.isActive) {
+		return undefined;
+	}
+	return gitExtension.exports.getAPI(1);
 }
 
 export function getGitHubUrl(
@@ -36,10 +36,10 @@ export function getGitHubUrl(
 		return undefined;
 	}
 
-	const remote = repo.state.remotes.find((r: any) => r.name === 'origin');
-	const remoteUrl = remote?.fetchUrl ?? remote?.pushUrl;
-	if (!remoteUrl) {
-		log('getGitHubUrl: no remote URL found');
+	const origin = repo.state.remotes.find((r: any) => r.name === 'origin');
+	const originUrl = origin?.fetchUrl ?? origin?.pushUrl;
+	if (!originUrl) {
+		log('getGitHubUrl: no remote "origin" URL found');
 		return undefined;
 	}
 
@@ -49,7 +49,7 @@ export function getGitHubUrl(
 		return undefined;
 	}
 
-	const baseUrl = remoteUrlToHttps(remoteUrl);
+	const baseUrl = remoteUrlToHttps(originUrl);
 	const url = `${baseUrl}/blob/${commitHash}/${filePath}#L${startLine}-L${endLine}`;
 	log(`getGitHubUrl: ${url}`);
 	return url;
